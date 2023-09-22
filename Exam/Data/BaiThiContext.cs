509d0a30b696082d9771d89b4366f4f8e8ff54a4
minhkhanhh <102962225+minhkhanhh@users.noreply.github.com>
2023-09-22 22:43:45 +0700

    Tran Bui thanh

---
+++ Exam/Data/BaiThiContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BaiThi.Models;
+
+    public class BaiThiContext : DbContext
+    {
+        public BaiThiContext (DbContextOptions<BaiThiContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BaiThi.Models.Department> Department { get; set; } = default!;
+
+        public DbSet<BaiThi.Models.Employee>? Employee { get; set; }
+    }
--- Test22_9/Test22_9/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Test22_9.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
