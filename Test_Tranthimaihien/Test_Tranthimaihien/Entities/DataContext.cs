e99b3fa4595226a470fdb076c2d7aa930a61fea2
Hien Tran <109644638+hienttm@users.noreply.github.com>
2023-09-22 18:52:09 +0700

    test_commit
    up

---
+++ Test_Tranthimaihien/Test_Tranthimaihien/Entities/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace Test_Tranthimaihien.Entities
+{
+	public class DataContext:DbContext
+	{
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+    }
+}
+
