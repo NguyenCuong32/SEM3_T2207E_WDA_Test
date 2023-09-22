cbe18955fe903478fe41fe90604132a167cd8d38
DEv021003 <quangtu0210@gmail.com>
2023-09-22 20:14:20 +0700

    :(

---
+++ SEM3_T2207E_WDA/Pages/Index.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace SEM3_T2207E_WDA.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
