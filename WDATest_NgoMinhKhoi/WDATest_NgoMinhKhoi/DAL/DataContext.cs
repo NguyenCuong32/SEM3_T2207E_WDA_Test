2c4a6f7d48dcd7341bd0ba1471b6be6935b7e139
NgoMinhKhoi2603 <109575681+NgoMinhKhoi2603@users.noreply.github.com>
2023-09-22 18:53:54 +0700

    TEST

---
+++ WDATest_NgoMinhKhoi/WDATest_NgoMinhKhoi/DAL/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WDATest_NgoMinhKhoi.DAL
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ WDATest_NgoMinhKhoi/WDATest_NgoMinhKhoi/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WDATest_NgoMinhKhoi.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
